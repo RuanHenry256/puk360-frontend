c591e9cb966dcf934b18981317cd6740a0bb8bec
Kamogelo24 <kaukamogelo01@gmail.com>
2025-09-24 20:15:50 +0200

    Hey I did my part but it under componebts (EventDetail)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import LoginScreen from './LoginScreen'; // Make sure the path is correct
 import './App.css';
+import EventDetail from "./components/EventDetail";
 
 function App() {
   return (
     <div className="App">
-      <LoginScreen />
-    </div>
+     <LoginScreen />
+     <EventDetail />
+  </div>
   );
 }
 
